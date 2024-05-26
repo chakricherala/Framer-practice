93d65382dbf0a8151fd212f8a1e0bfe4cb667502
Chakradhar Cherala <68188570+chakricherala@users.noreply.github.com>
2024-05-26 20:48:50 +0530

    Framer tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import AnimationControls from "../components/Animationcontrols";
+import AnimationControls from "../components/AnimationControls";
 import BasicsOfMotion from "../components/BasicsOfMotion";
 import Gestures from "../components/Gestures";
 import ScrollAnimations from "../components/ScrollAnimation";
